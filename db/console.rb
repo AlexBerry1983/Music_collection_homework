fc9e5fcbd80d9c4f72c24ccd462afe7237848923
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-04-26 16:47:24 +0100

    console and music collection.sql added

--- db/console.rb
+++ db/console.rb
@@ -2,3 +2,42 @@ require('pry-byebug')
 require_relative('../models/albums')
 require_relative('../models/artists')
 
+artist1 = Artist.new({ 'name' => 'Little Dragon'})
+artist1.save
+
+artist2 = Artist.new({ 'name' => 'Banks'})
+artist2.save
+
+album1 = Album.new({ 
+  'title' => 'Ritual Union',
+  'genre' => 'Electronic',
+  'artist_id' => artist1.id
+  })
+
+album1.save
+
+album2 = Album.new({
+  'title' => 'Nabuma Rubberband',
+  'genre' => 'Electronic',
+  'artist_id' => artist1.id
+  })
+
+album2.save
+
+album3 = Album.new({
+  'title' => 'Goddess',
+  'genre' => 'Trip hop',
+  'artist_id' => artist2.id
+  })
+
+album3.save
+
+album4 = Album.new({
+  'title' => 'The Altar',
+  'genre' => 'Alternative R&B',
+  'artist_id' => artist2.id
+  })
+
+album4.save
+binding.pry
+nil
